86f58b5338026693d3cb757babf00caa6809f319
Mykola Stesiuk <mikalaj.krutak@gmail.com>
2024-11-19 18:09:16 +0100

    final code for reviev

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write your code here
+const values = document.querySelectorAll('.population');
+const avarage = document.querySelector('.average-population');
+const total = document.querySelector('.total-population');
+let totl = 0;
+let items = 0;
+
+if (avarage && total) {
+  for (let i = 0; i < values.length; i++) {
+    const value = parseFloat(values[i].textContent);
+
+    if (!isNaN(value)) {
+      totl += value;
+      items++;
+    }
+  }
+
+  const avg = Math.round(totl / items);
+
+  avarage.textContent = avg;
+
+  total.textContent = totl;
+}
